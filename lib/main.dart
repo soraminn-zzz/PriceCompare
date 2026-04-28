a5d50f4d98b4a0eef99582fa9ba608b7c9f56e17
soraminn-zzz <soracoco10031219@gmail.com>
2026-04-28 11:44:26 +0900

    feat setting save

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:price_compare/compare_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,22 +18,51 @@ class _MyAppState extends State<MyApp> {
   String? _backgroundImagePath;
   double _cardOpacity = 1.0;
 
-  void _toggleTheme(ThemeMode mode) {
+  @override
+  void initState() {
+    super.initState();
+    _loadSetting();
+  }
+
+  Future<void> _loadSetting() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      final themeIndex = prefs.getInt('themeMode') ?? 0;
+      _themeMode = ThemeMode.values[themeIndex];
+
+      _backgroundImagePath = prefs.getString('backgroundImagePath');
+
+      _cardOpacity = prefs.getDouble('cardOpacity') ?? 1.0;
+    });
+  }
+
+  void _toggleTheme(ThemeMode mode) async {
     setState(() {
       _themeMode = mode;
     });
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('themeMode', mode.index);
   }
 
-  void _setBackground(String? path) {
+  void _setBackground(String? path) async {
     setState(() {
       _backgroundImagePath = path;
     });
+    final prefs = await SharedPreferences.getInstance();
+    if (path != null) {
+      await prefs.setString('backgroundImagePath', path);
+    } else {
+      await prefs.remove('backgroundImagePath');
+    }
   }
 
-  void _setCardOpacity(double value) {
+  void _setCardOpacity(double value) async {
     setState(() {
       _cardOpacity = value;
     });
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('cardOpacity', value);
   }
 
   @override
