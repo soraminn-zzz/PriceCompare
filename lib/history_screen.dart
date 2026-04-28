9c4713fdd2a095a8db9318ce422242af6eefd8ed
soraminn-zzz <soracoco10031219@gmail.com>
2026-04-28 11:50:27 +0900

    feat historyFile

---
+++ lib/history_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+class HistoryScreen extends StatefulWidget {
+  const HistoryScreen({super.key});
+
+  @override
+  State<HistoryScreen> creatState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+
+}
